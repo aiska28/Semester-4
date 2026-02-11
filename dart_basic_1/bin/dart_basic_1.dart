b92f99730c9abf4d06323bc7d340e20257420d6d
aiska28 <aiskacaca8@gmail.com>
2026-02-11 15:39:00 +0700

    first commit

---
+++ dart_basic_1/bin/dart_basic_1.dart
@@ -0,0 +1,8 @@
+import 'package:dart_basic_1/dart_basic_1.dart' as dart_basic_1; //menyertakan file(include)
+
+//program utama
+void main(List<String> arguments) {
+  print('Hello world: ${dart_basic_1.calculate()}!');
+
+  print('Luas Segi Tiga: ${dart_basic_1.calculateSegitiga(5,4)}!');
+}
---
+++ dart_basic_1/lib/dart_basic_1.dart
@@ -0,0 +1,9 @@
+//tambahan, seperti fungsi
+
+int calculate() {
+  return 6 * 7;
+}
+
+double calculateSegitiga(double luas, double tinggi) {
+  return 0.5 * luas * tinggi;
+}
